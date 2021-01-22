4a9e14afcbadad5b78748d2e823b9e3f63990d78
ethan-stone <ethan.stone9352@gmail.com>
2021-01-22 18:57:49 -0500

    refactored colored list item to remove unnecessary computed property

--- src/components/UI/ColoredListItem.vue
+++ src/components/UI/ColoredListItem.vue
@@ -1,11 +1,14 @@
 <template>
-  <li class="inset-x-0 text-sm mx-2 mt-2 rounded-sm" :class="cssClass">
+  <li class="inset-x-0 text-sm mx-2 mt-2 rounded-sm" 
+      :class="['bg-' + color + '-' + bgIntensity, 'text-' + color + '-' + textIntensity]">
     <p class="mx-1 font-bold">{{ itemHeader }}</p>
     <p class="mx-1">{{ itemText }}</p>
   </li>
 </template>
 
 <script>
+import { computed } from "vue"
+
 export default {
   name: "ColoredListItem",
   props: {
@@ -14,14 +17,6 @@ export default {
     textIntensity: Number,
     itemHeader: String,
     itemText: String
-  },
-  computed: {
-    cssClass() {
-      return [
-        "bg-" + this.color + "-" + this.bgIntensity,
-        "text-" + this.color + "-" + this.textIntensity
-      ];
-    }
   }
 };
 </script>
