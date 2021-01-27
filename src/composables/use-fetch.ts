d20f48ba3bfe4e5f29cf07c2c5c064d342aa785c
ethan-stone <ethan.stone9352@gmail.com>
2021-01-27 01:11:27 -0500

    create useFetch composable

---
+++ src/composables/use-fetch.ts
@@ -0,0 +1,24 @@
+import { toRefs, reactive } from "vue";
+
+export default function useFetch(url: string, options: any) {
+  const state = reactive({
+    response: [],
+    error: null,
+    fetching: false
+  });
+
+  async function fetchFunc() {
+    state.fetching = true;
+    try {
+      const res = await fetch(url, options);
+      const json = await res.json();
+      state.response = json;
+    } catch (errors) {
+      state.error = errors;
+    } finally {
+      state.fetching = false;
+    }
+  }
+
+  return { ...toRefs(state), fetchFunc };
+}
