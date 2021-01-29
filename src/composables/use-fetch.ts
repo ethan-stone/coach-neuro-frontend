1851a9fb83bbfcb94dbcec133beb48363c75c5e1
ethan-stone <ethan.stone9352@gmail.com>
2021-01-29 02:03:11 -0500

    created request flow with auth

--- src/composables/use-fetch.ts
+++ src/composables/use-fetch.ts
@@ -1,23 +1,51 @@
 import { toRefs, reactive } from "vue";
+import FetchOptions from "../interfaces/fetchOptions";
+import { refreshAccessToken } from "./use-auth";
 
-export default function useFetch(url: string, options: any) {
+export default function useFetch(url: string, options: FetchOptions) {
   const state = reactive({
     response: [],
     error: null,
-    fetching: false
+    fetching: false,
+    isSuccess: false
   });
+
+  async function tryFetch() {
+    var isSuccess = false;
+    await fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        isSuccess = true;
+        state.response = data;
+      })
+      .catch((error) => {
+        state.error = error;
+      });
+    return isSuccess;
+  }
 
   async function fetchData() {
     state.fetching = true;
-    try {
-      const res = await fetch(url, options);
-      const json = await res.json();
-      state.response = json;
-    } catch (errors) {
-      state.error = errors;
-    } finally {
-      state.fetching = false;
+
+    // try request with current access token
+    const isInitialSuccess = await tryFetch();
+    state.isSuccess = isInitialSuccess;
+
+    if (!isInitialSuccess) {
+      // if not successful refresh the token
+      const isRefreshSuccess = await refreshAccessToken();
+
+      // if refresh is not successful force logout to login again
+      if (!isRefreshSuccess) {
+        // force logout
+      }
+
+      // retry request
+      const isRetrySuccess = await tryFetch();
+      state.isSuccess = isRetrySuccess;
     }
+    // final result of state.isSuccess will be handled by caller of fetchData
+    // caller will display appropiate error message
   }
 
   return { ...toRefs(state), fetchData };
---
+++ src/interfaces/fetchOptions.ts
@@ -0,0 +1,4 @@
+export default interface FetchOptions {
+  method: string;
+  body: string;
+}
