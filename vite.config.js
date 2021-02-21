0708642478482fe0dbaba7cf4d90fd3a9f6e080d
ethan-stone <ethan.stone9352@gmail.com>
2021-02-21 05:39:49 -0500

    added https for development

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+const fs = require("fs");
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
-}
+  plugins: [vue()],
+  server: {
+    https: {
+      key: fs.readFileSync("mkcert_localhost/key.pem"),
+      cert: fs.readFileSync("mkcert_localhost/cert.pem")
+    }
+  }
+};
