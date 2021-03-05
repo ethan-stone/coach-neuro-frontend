d771b76e4cd5bdb75f48b71bde1e0d89c0533086
ethan-stone <ethan.stone9352@gmail.com>
2021-03-05 16:43:55 -0500

    moving to firebase

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTJZwkehj-5FKW2oV1kiDj0HFnH9vbwY4",
+  authDomain: "coachneuro.firebaseapp.com",
+  projectId: "coachneuro",
+  storageBucket: "coachneuro.appspot.com",
+  messagingSenderId: "405525062820",
+  appId: "1:405525062820:web:ef03f911b40a91ae03f231",
+  measurementId: "G-BKF04767N4"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const auth = firebaseApp.auth();
+const db = firebaseApp.firestore();
+
+export { firebase, auth, db };
