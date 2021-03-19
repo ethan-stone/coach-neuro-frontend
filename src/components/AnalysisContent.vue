5d9ef38fd3756c10977701a0142875e48aa14060
ethan-stone <ethan.stone9352@gmail.com>
2021-03-19 01:43:28 -0400

    started layout of analysis page

--- src/components/AnalysisContent.vue
+++ src/components/AnalysisContent.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="h-screen w-full">
-    <video
-      :src="downloadURL"
-      ref="videoElementRef"
-      crossorigin="anonymous"
-      autoplay
-      muted="true"
-    />
-    <button @click="estimateVideoPoses">Analyze Video</button>
+    <div class="grid grid-cols-2 md:grid-cols-3 gap-10 p-10">
+      <video
+        :src="downloadURL"
+        ref="videoElementRef"
+        crossorigin="anonymous"
+        autoplay
+        muted="true"
+        class="bg-black p-1 rounded col-span-2"
+      />
+      <div class="grid grid-cols-1 gap-10">
+        <div class="self-center">
+          <p class="pr-2 text-lg font-bold">Elbow</p>
+          <div
+            class="h-6 bg-gradient-to-r from-green-200 to-green-700 rounded-lg"
+            style="width: 100%"
+          ></div>
+        </div>
+        <div class="self-center">
+          <p class="pr-2 text-lg font-bold">Legs</p>
+          <div
+            class="h-6 bg-gradient-to-r from-yellow-200 to-yellow-500 rounded-lg"
+            style="width: 66%"
+          ></div>
+        </div>
+        <div class="self-center">
+          <p class="pr-2 text-lg font-bold">wefwef</p>
+          <div
+            class="h-5 bg-gradient-to-r from-red-200 to-red-600 rounded-lg"
+            style="width: 33%"
+          ></div>
+        </div>
+      </div>
+      <div class="flex md:col-span-full justify-center">
+        <button
+          @click="estimateVideoPoses"
+          class="m-auto p-3 bg-black text-white rounded"
+        >
+          Analyze Video
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
