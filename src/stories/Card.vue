142a298030e8841cab68967102cdc81385c8b58d
ethan-stone <ethan.stone9352@gmail.com>
2021-04-16 23:43:24 -0400

    getting started with storybook

---
+++ src/stories/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, computed } from "vue";
+import "../index.css";
+
+export default defineComponent({
+  name: "Card",
+  props: {
+    width: {
+      type: String,
+      default: "full"
+    },
+    maxWidth: {
+      type: String,
+      default: "full"
+    },
+    height: {
+      type: String,
+      default: "full"
+    },
+    maxHeight: {
+      type: String,
+      default: "full"
+    },
+    shadow: {
+      type: String,
+      default: "none"
+    }
+  },
+  setup(props) {
+    props = reactive(props);
+    return {
+      classes: computed(() => {
+        return [
+          "w-" + props.width,
+          "max-w-" + props.maxWidth,
+          "h-" + props.height,
+          "max-h-" + props.maxHeight,
+          "shadow-" + props.shadow
+        ];
+      })
+    };
+  }
+});
+</script>
---
+++ src/stories/Grid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="grid" :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, computed } from "vue";
+import "../index.css";
+
+export default defineComponent({
+  name: "Grid",
+  props: {
+    colsSmall: {
+      type: String,
+      required: true
+    },
+    colsMedium: {
+      type: String,
+      required: true
+    },
+    colsLarge: {
+      type: String,
+      required: true
+    },
+    gap: {
+      type: String,
+      default: 0
+    }
+  },
+  setup(props) {
+    props = reactive(props);
+    return {
+      classes: computed(() => {
+        return [
+          "grid-cols-" + props.colsSmall,
+          "md:grid-cols-" + props.colsMedium,
+          "lg:grid-cols-" + props.colsLarge,
+          "gap-" + props.gap
+        ];
+      })
+    };
+  }
+});
+</script>
